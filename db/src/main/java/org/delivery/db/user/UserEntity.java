b205888d0d537e4103a6cf6fb92d63e34d46280f
박하민 <parkhamint@naver.com>
2023-12-21 03:49:06 +0900

    UserEntity 개발

---
+++ db/src/main/java/org/delivery/db/user/UserEntity.java
@@ -0,0 +1,43 @@
+package org.delivery.db.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.delivery.db.BaseEntity;
+import org.delivery.db.user.enums.UserStatus;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public class UserEntity extends BaseEntity {
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 100, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 50, nullable = false)
+    private UserStatus status;
+
+    @Column(length = 100, nullable = false)
+    private String address;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private LocalDateTime lastLoginAt;
+}
